e070c0af00b76c75cdec19e0bcee4ac2833d4aa9
Christopher Ponzio <chrisponz11@yahoo.com>
2022-02-27 12:13:13 -0500

    more atarter code and starter prompts added

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the project title?',
+        },
+        {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a short description of your project.',
+        },
+        {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are the steps required to install your project?',
+        },
+        {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide instructions and examples for use.',
+        },
+        {
+        type: 'input',
+        name: 'Credits',
+        message: 'List of collaborators and 3rd party assets.',
+        },
+        {
+        type: 'input',
+        name: 'License',
+        message: 'What is the license type?',
+        },
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

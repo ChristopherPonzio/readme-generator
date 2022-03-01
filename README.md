64f534a25b8ab210522c36f3f07ab984d3752097
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-01 17:06:47 -0500

    added License Badge

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-# t
 
-      ## Description
-      t
+  # Apple
 
-      ## Installation
-      t
+  ![Github license](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
 
-      ## Usage
-      t
-
-      ## Credits
-      t
-
-      ## License
-      t
+  ## Description
+  Banna
+  
+  ## Installation
+  Orange
+  
+  ## Usage
+  Pinapple
+  
+  ## Credits
+  MonkFruit
+  
+  ## License
+      Licensed under the MIT license.
+  https://choosealicense.com/licenses/MIT
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,34 +1,58 @@
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `https://choosealicense.com/licenses/${license}`
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License
+      Licensed under the ${license} license.`
+
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-      ## Description
-      ${data.description}
+  ${renderLicenseBadge(data.license)}
 
-      ## Installation
-      ${data.installation}
-
-      ## Usage
-      ${data.usage}
-
-      ## Credits
-      ${data.credits}
-
-      ## License
-      ${data.license}
+  ## Description
+  ${data.description}
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Credits
+  ${data.credits}
+  
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+  
 `;
 }
 

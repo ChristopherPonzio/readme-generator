11dd3a99ae636462ea22db8837e2f7b1c02f41a8
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-05 13:15:19 -0500

    misc generate markdown changes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {

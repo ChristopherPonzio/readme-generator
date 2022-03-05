07a54123b5fa4954215a28dc82db9afa7cdc1f3d
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-05 13:16:51 -0500

    misc generateMarkdown changes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
   }
   return ''
 }
